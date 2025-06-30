1d01c5f689d97d7c930112d16048520183257483
syaquiii <msyauqioqi@gmail.com>
2025-06-30 22:05:30 +0700

    feat: add authentication and authorization

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginPageContainer from "@/feature/(auth)/login/container/LoginPageContainer";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <LoginPageContainer />
+    </>
+  );
+};
+
+export default page;
