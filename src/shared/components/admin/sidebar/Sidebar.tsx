4e3eb0d80dac0dcd8efbb6a8f830196797816efb
syaquiii <msyauqioqi@gmail.com>
2025-07-01 21:55:28 +0700

    feat: add sidebar admin

--- src/shared/components/admin/sidebar/Sidebar.tsx
+++ src/shared/components/admin/sidebar/Sidebar.tsx
@@ -3,9 +3,14 @@
 import { useAuthContext } from "@/shared/hooks/useAuthContext";
 import { authService } from "@/api/services/auth";
 import React from "react";
+import { sidebarMenuItems } from "@/shared/data/SidebarData";
+import Link from "next/link";
+import { LogOut } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
   const { user } = useAuthContext();
+  const pathname = usePathname();
 
   const handleLogout = async () => {
     try {
@@ -16,27 +21,44 @@ const Sidebar = () => {
   };
 
   return (
-    <div className="bg-white border-r-2 border-gray-200 flex flex-col justify-between p-4 w-64 h-screen">
-      <div>
-        <div className="text-center mb-10">
-          <h2 className="text-lg font-bold">Admin Panel</h2>
-          <p className="text-sm text-gray-600">Selamat Datang,</p>
-          <p className="font-semibold">{user?.nama}</p>
-        </div>
-        <nav>
-          <ul>
-            <li className="p-2 hover:bg-gray-100 rounded-md">Dashboard</li>
-            <li className="p-2 hover:bg-gray-100 rounded-md">A</li>
-            <li className="p-2 hover:bg-gray-100 rounded-md">B</li>
-          </ul>
-        </nav>
+    <div className=" border-r bg-white relative border-gray-200 shadow-xl flex flex-col px-5 py-7 w-80 h-screen">
+      <div className="flex items-center gap-3">
+        <div className="bg-gray-500 w-16 h-16 rounded-xl"></div>
+        <p className="text-sm w-3/4 font-semibold">
+          Synergy Of Symphony & Shaping The Future
+        </p>
       </div>
+      <div className="mt-4 px-4 py-2 bg-gray-200 rounded-lg">
+        <span className="text-gray-700 font-medium">
+          Welcome, Admin {user?.nama}!
+        </span>
+      </div>
+      <ul className="mt-18 flex flex-col gap-5">
+        {sidebarMenuItems.map((sidebar) => {
+          const isActive = pathname === sidebar.path;
 
-      <div>
+          return (
+            <Link key={sidebar.id} href={sidebar.path} passHref>
+              <li
+                className={`px-4 py-5 flex gap-x-3 text-lg transition-all duration-300 rounded-xl ${
+                  isActive
+                    ? "bg-primary-500 text-white"
+                    : "text-primary-500 hover:bg-primary-500 hover:text-white"
+                }`}
+              >
+                {React.createElement(sidebar.icon)}
+                {sidebar.label}
+              </li>
+            </Link>
+          );
+        })}
+      </ul>
+      <div className="absolute bottom-0 h-20 w-full left-0">
         <button
           onClick={handleLogout}
-          className="w-full bg-red-500 text-white p-2 rounded-md hover:bg-red-600 transition-colors"
+          className="w-full flex gap-2 items-center justify-center bg-primary-500 h-full text-white p-2 hover:bg-red-600 transition-colors"
         >
+          <LogOut />
           Logout
         </button>
       </div>
